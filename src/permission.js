f17a6c46812bb174ee3b82c5fc6e1786d1959cfa
李大炮 <2622268246@qq.com>
2022-09-26 10:59:41 +0800

    侧边栏样式完成

---
+++ src/permission.js
@@ -0,0 +1,29 @@
+import router from './router'
+import store from '@/store'
+
+const whiteList = ['/login', '/404']
+
+router.beforeEach((to, from, next) => {
+  // 判断token是否存在
+  // 存在 登录状态
+  if (store.getters.token) {
+    // 判断是否去登录页面
+    if (to.path === '/login') {
+      // 留在首页
+      next('/')
+    } else {
+      // 允许去往首页
+      next()
+    }
+  } else {
+    // 不存在 未登录状态
+    // 如果在白名单内
+    if (whiteList.includes(to.path)) {
+      // 放行
+      next()
+    } else {
+      // 否则留在登录页面
+      next('/login')
+    }
+  }
+})

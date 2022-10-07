7c7d1574c035c45f3428a3abe246ddc68ab68fc7
李大炮 <2622268246@qq.com>
2022-10-07 15:06:07 +0800

    一个通用的组件

---
+++ src/components/index.js
@@ -0,0 +1,17 @@
+// require.context(路径，是否查看子目录，正则) ==》返回一个函数
+// require.context 返回的函数 有一个keys属性，对应也是一个方法，返回值 找到内容的路径
+// require.context 返回的函数 的作用 根据路径 找到对应的模块
+const fn = require.context('./', true, /\.vue$/)
+// console.log(fn.keys())
+// console.log(fn('./PageTools/index.vue'))
+
+const components = fn.keys().map(ele => {
+  return fn(ele)
+})
+
+export default (Vue) => {
+  components.forEach(ele => {
+    // console.log(ele)
+    Vue.component(ele.default.name, ele.default)
+  })
+}
--- src/views/employees/index.vue
+++ src/views/employees/index.vue
@@ -1,13 +1,23 @@
 <template>
   <div>
-    employees
+    <PageTools type="error">
+      <template #before>
+        <span>共25条记录</span>
+      </template>
+
+      <template #after>
+        <el-button size="small" type="warning">导入</el-button>
+        <el-button size="small" type="danger">导出</el-button>
+      </template>
+    </PageTools>
   </div>
 </template>
 
 <script>
 export default {
   name: 'HrsaasIndex',
-
+  components: {
+  },
   data() {
     return {
 

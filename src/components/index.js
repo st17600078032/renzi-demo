a1206a53a20cd94af0d6661d746819e1f37e45db
李大炮 <2622268246@qq.com>
2022-10-10 10:03:55 +0800

    1

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,5 @@
+import * as filters from '@/filters'
+
 // require.context(路径，是否查看子目录，正则) ==》返回一个函数
 // require.context 返回的函数 有一个keys属性，对应也是一个方法，返回值 找到内容的路径
 // require.context 返回的函数 的作用 根据路径 找到对应的模块
@@ -13,5 +15,9 @@ export default (Vue) => {
   components.forEach(ele => {
     // console.log(ele)
     Vue.component(ele.default.name, ele.default)
+  })
+
+  Object.keys(filters).forEach(key => {
+    Vue.filter(key, filters[key])
   })
 }
--- src/views/employees/index.vue
+++ src/views/employees/index.vue
@@ -1,35 +1,108 @@
 <template>
-  <div>
-    <PageTools type="error">
+  <div class="app-container">
+    <PageTools>
       <template #before>
-        <span>共25条记录</span>
+        <span>共166条记录</span>
       </template>
-
       <template #after>
         <el-button size="small" type="warning">导入</el-button>
         <el-button size="small" type="danger">导出</el-button>
+        <el-button size="small" type="primary" @click="handleAdd">新增员工</el-button>
       </template>
     </PageTools>
+    <!-- 放置表格和分页 -->
+    <el-card>
+      <el-table v-loading="loading" border :data="list">
+        <el-table-column label="序号" sortable="" width="80" type="index" />
+        <el-table-column label="姓名" prop="username" />
+        <el-table-column label="工号" prop="workNumber" />
+        <el-table-column label="聘用形式" prop="formOfEmployment" :formatter="formatterFn" />
+        <el-table-column label="部门" prop="departmentName" />
+        <el-table-column label="入职时间" sortable="" prop="timeOfEntry">
+          <template slot-scope="{row}">
+            {{ row.timeOfEntry | formatDate }}
+          </template>
+        </el-table-column>
+        <el-table-column label="账户状态" prop="enableState">
+          <template slot-scope="{ row }">
+            <!-- 根据当前状态来确定 是否打开开关 -->
+            <el-switch :value="row.enableState === 1" />
+          </template>
+        </el-table-column>
+        <el-table-column label="操作" fixed="right" width="280">
+          <template>
+            <el-button type="text" size="small">查看</el-button>
+            <el-button type="text" size="small">转正</el-button>
+            <el-button type="text" size="small">调岗</el-button>
+            <el-button type="text" size="small">离职</el-button>
+            <el-button type="text" size="small">角色</el-button>
+            <el-button type="text" size="small">删除</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+      <!-- 分页组件 -->
+      <el-row type="flex" justify="end" align="middle" style="height: 60px">
+        <el-pagination
+          background
+          layout="prev, pager, next, sizes, total"
+          :current-page.sync="page.page"
+          :page-size.sync="page.size"
+          :page-sizes="[5,10,15,20]"
+          :total="total"
+          @current-change="getEmployeeList"
+          @size-change="getEmployeeList"
+        />
+      </el-row>
+    </el-card>
+    <add-employee :dialog-visible.sync="showDialog" />
   </div>
 </template>
 
 <script>
+import { getEmployeeList } from '@/api/employees'
+import EnumHireType from '@/api/constant/employees'
+import addEmployee from './components/add-employee'
 export default {
   name: 'HrsaasIndex',
   components: {
+    addEmployee
   },
   data() {
     return {
-
+      showDialog: false,
+      page: {
+        page: 1,
+        size: 10
+      },
+      list: [], // 接数据的
+      total: 0, // 总数
+      loading: false,
+      hireType: EnumHireType.hireType
     }
   },
 
   mounted() {
-
+    this.getEmployeeList()
   },
 
   methods: {
-
+    async getEmployeeList() {
+      try {
+        this.loading = true
+        const { rows, total } = await getEmployeeList(this.page)
+        this.list = rows
+        this.total = total
+      } finally {
+        this.loading = false
+      }
+    },
+    formatterFn(row, column, cellValue) {
+      const res = this.hireType.find(ele => ele.id === cellValue)
+      return res.value
+    },
+    handleAdd() {
+      this.showDialog = true
+    }
   }
 }
 </script>
